6e71b9676bf15107d454e0d8e46361246e3cda7f
Olaf Hering <olaf@aepfle.de>
2021-01-01 00:00:00 +0000

    wip
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- src/oauth2.c
+++ src/oauth2.c
@@ -274,6 +274,7 @@ static gchar *oauth2_contact_server(SockInfo *sock, const gchar *request)
 	if (timeout)
 		log_message(LOG_PROTOCOL, _("OAuth2 socket timeout error\n"));
 
+	debug_print("Response from server: %s\n", response->str);
 	return g_string_free(response, got_some_error || timeout);
 }
 
@@ -392,7 +393,6 @@ int oauth2_obtain_tokens (Oauth2Service provider, OAUTH2Data *OAUTH2Data, const 
 
 	oauth2_post_request (request, OAUTH2info[i][OA2_BASE_URL], OAUTH2info[i][OA2_ACCESS_RESOURCE], header, body);
 	response = oauth2_contact_server (sock, request);
-	debug_print("Response from server: %s\n", response);
 
 	if(response && oauth2_filter_access (response, access_token, &expiry) == 0){
 	  OAUTH2Data->access_token = g_strdup(access_token);
@@ -527,9 +527,7 @@ gint oauth2_use_refresh_token (Oauth2Service provider, OAUTH2Data *OAUTH2Data)
 	}
 
 	oauth2_post_request (request, OAUTH2info[i][OA2_BASE_URL], OAUTH2info[i][OA2_REFRESH_RESOURCE], header, body);
-	debug_print("Request: %s\n", request);
 	response = oauth2_contact_server (sock, request);
-	debug_print("Response from server: %s\n", response);
 
 
 	if(response && oauth2_filter_access (response, access_token, &expiry) == 0){
