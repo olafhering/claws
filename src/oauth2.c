62890bfb812d0781c53acec04507fd29a4e9babe
Olaf Hering <olaf@aepfle.de>
2022-05-10 16:21:40 +0000

    Entfernung leerer Zeichenketten für OA2_STATE
    Ob OA2_STATE für die URL nötig ist lässt sich auch dadurch
    testen ob eine Zeichenkette dafür existiert.
    
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- src/oauth2.c
+++ src/oauth2.c
@@ -60,7 +60,6 @@ static gchar *OAUTH2info[OAUTH2AUTH_LAST - 1][OA2_LAST] = {
 		[OA2_SCOPE_FOR_AUTH] = "https://mail.google.com",
 		[OA2_GRANT_TYPE_ACCESS] = "authorization_code",
 		[OA2_GRANT_TYPE_REFRESH] = "refresh_token",
-		[OA2_STATE] = "",
 		[OA2_ACCESS_TYPE] = "",
 		[OA2_RESPONSE_MODE] = "",
 		[OA2_HEADER_AUTH_BASIC] = "",
@@ -77,7 +76,6 @@ static gchar *OAUTH2info[OAUTH2AUTH_LAST - 1][OA2_LAST] = {
 		[OA2_GRANT_TYPE_ACCESS] = "authorization_code",
 		[OA2_GRANT_TYPE_REFRESH] = "refresh_token",
 		[OA2_TENANT] = "common",
-		[OA2_STATE] = "",
 		[OA2_ACCESS_TYPE] = "offline",
 		[OA2_SCOPE_FOR_ACCESS] = "offline_access https://outlook.office.com/IMAP.AccessAsUser.All https://outlook.office.com/POP.AccessAsUser.All https://outlook.office.com/SMTP.Send",
 		[OA2_RESPONSE_MODE] = "query",
@@ -114,7 +112,6 @@ static gchar *OAUTH2info[OAUTH2AUTH_LAST - 1][OA2_LAST] = {
 		[OA2_SCOPE_FOR_AUTH] = "",
 		[OA2_GRANT_TYPE_ACCESS] = "authorization_code",
 		[OA2_GRANT_TYPE_REFRESH] = "refresh_token",
-		[OA2_STATE] = "",
 		[OA2_ACCESS_TYPE] = "",
 		[OA2_RESPONSE_MODE] = "",
 		[OA2_HEADER_AUTH_BASIC] = "1",
@@ -354,7 +351,7 @@ int oauth2_obtain_tokens(Oauth2Service provider, OAUTH2Data *OAUTH2Data, const g
 		g_free(body);
 		body = tmp;
 	}
-	if (OAUTH2info[i][OA2_STATE][0]) {
+	if (OAUTH2info[i][OA2_STATE]) {
 		tmp = g_strconcat(body, "&state=", OAUTH2info[i][OA2_STATE], NULL);
 		g_free(body);
 		body = tmp;
@@ -478,7 +475,7 @@ static gint oauth2_use_refresh_token(Oauth2Service provider, OAUTH2Data *OAUTH2D
 		g_free(uri);
 		body = tmp;
 	}
-	if (OAUTH2info[i][OA2_STATE][0]) {
+	if (OAUTH2info[i][OA2_STATE]) {
 		uri = g_uri_escape_string(OAUTH2info[i][OA2_STATE], NULL, FALSE);
 		tmp = g_strconcat(body, "&state=", uri, NULL);
 		g_free(body);
@@ -586,7 +583,7 @@ gchar *oauth2_authorisation_url(Oauth2Service provider, const gchar *custom_clie
 		g_string_append(url, tmp);
 		g_free(tmp);
 	}
-	if (OAUTH2info[i][OA2_STATE][0]) {
+	if (OAUTH2info[i][OA2_STATE]) {
 		tmp = g_uri_escape_string(OAUTH2info[i][OA2_STATE], NULL, FALSE);
 		g_string_append(url, "&state=");
 		g_string_append(url, tmp);
