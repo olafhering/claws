a2255922fc9fe7afedc6cbb25cf36d172841d282
Paul <paul@claws-mail.org>
2023-11-17 15:27:17 +0000

    fix bug 4361, 'Embedding external editor crashes Claws-Mail on Wayland' and a similar crash with the dillo plugin

--- src/plugins/dillo/dillo_viewer.c
+++ src/plugins/dillo/dillo_viewer.c
@@ -1,6 +1,6 @@
 /*
- * Claws Mail -- a GTK+ based, lightweight, and fast e-mail client
- * Copyright (C) 1999-2012 the Claws Mail Team
+ * Claws Mail -- a GTK based, lightweight, and fast e-mail client
+ * Copyright (C) 1999-2023 the Claws Mail Team
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -122,6 +122,11 @@ static void dillo_show_mimepart(MimeViewer *_viewer,
 	if (viewer->filename != NULL) {
 		claws_unlink(viewer->filename);
 		g_free(viewer->filename);
+	}
+
+	if (!GDK_IS_X11_DISPLAY(gdk_display_get_default())) {
+		debug_print("dillo viewer only works on X11\n");
+		return;
 	}
 
 	viewer->filename = procmime_get_tmp_file_name(partinfo);
