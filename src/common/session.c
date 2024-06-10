0948d4539489d047a11ec827c885cc3a406b0691
Olaf Hering <olaf@aepfle.de>
2024-06-10 10:42:14 +0000

    session: Korrektur von ping_tag
    Die Funktion g_timeout_add_seconds liefert eine ID vom Typ guint.
    
    Diese Änderung hätte Teil von 94b24847bf5cf842c388a710d59b85d256054e87
    sein müssen.
    
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- src/common/session.c
+++ src/common/session.c
@@ -90,8 +90,6 @@ void session_init(Session *session, const void *prefs_account)
 	session->data = NULL;
 	session->account = prefs_account;
 
-	session->ping_tag = -1;
-
 	session->proxy_info = NULL;
 }
 
@@ -855,10 +853,11 @@ void session_register_ping(Session *session, gboolean (*ping_cb)(gpointer data))
 {
 	if(!session)
 		return;
-	if (session->ping_tag > -1)
+	if (session->ping_tag) {
 		g_source_remove(session->ping_tag);
+		session->ping_tag = 0;
+	}
 
-	session->ping_tag = -1;
 
 	if (ping_cb != NULL)
 		session->ping_tag = g_timeout_add_seconds(60, ping_cb, session);
