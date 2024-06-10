7b6ea258c22f7a2038423a627c6854828465bdb3
Olaf Hering <olaf@aepfle.de>
2024-06-10 10:42:31 +0000

    socket: Entfernung von überflüssiger Überprüfung auf client_key in ssl_done_socket
    Die Funktion gnutls_x509_privkey_deinit überprüft die Werte seit
    gnutls_1_7_10~2993 selbst. Der Speicher wird ohnehin freigegeben.
    
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- src/common/ssl.c
+++ src/common/ssl.c
@@ -474,8 +474,7 @@ void ssl_done_socket(SockInfo *sockinfo)
 		gnutls_deinit(sockinfo->ssl);
 #if GNUTLS_VERSION_NUMBER < 0x030000
 		gnutls_x509_crt_deinit(sockinfo->client_crt);
-		if (sockinfo->client_key)
-			gnutls_x509_privkey_deinit(sockinfo->client_key);
+		gnutls_x509_privkey_deinit(sockinfo->client_key);
 		sockinfo->client_key = NULL;
 		sockinfo->client_crt = NULL;
 #else
