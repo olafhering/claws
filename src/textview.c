55c0ed3c4a19957ae59fa3961307feda96673874
Paul <paul@claws-mail.org>
2025-09-01 17:26:52 +0100

    probably more like what was expected from dfbff76ff30f2d8e3a4d26bf1394d1cf385c4460

--- src/textview.c
+++ src/textview.c
@@ -2427,6 +2427,8 @@ static gint textview_key_pressed(GtkWidget *widget, GdkEventKey *event,
 	case GDK_KEY_Down:
 	case GDK_KEY_Control_L:
 	case GDK_KEY_Control_R:
+	case GDK_KEY_KP_Down:
+	case GDK_KEY_KP_Up:
 		return FALSE;
 	case GDK_KEY_Home:
 	case GDK_KEY_End:
@@ -2443,12 +2445,10 @@ static gint textview_key_pressed(GtkWidget *widget, GdkEventKey *event,
 		}
 		break;
 	case GDK_KEY_Page_Down:
-	case GDK_KEY_KP_Down:
 		mimeview_scroll_page(messageview->mimeview, FALSE);
 		break;
 	case GDK_KEY_Page_Up:
 	case GDK_KEY_BackSpace:
-	case GDK_KEY_KP_Up:
 		mimeview_scroll_page(messageview->mimeview, TRUE);
 		break;
 	case GDK_KEY_Return:
