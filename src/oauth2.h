5e1bd2ebddddca0c89827dfd139c68c5d3b5804d
Olaf Hering <olaf@aepfle.de>
2026-02-12 22:27:02 +0000

    oauth2: Entfernen von oauth2_init
    Diese Funktion war unnötig weil OAUTH2Data nur Zeiger enthält.
    
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- src/oauth2.h
+++ src/oauth2.h
@@ -69,7 +69,6 @@ struct _OAUTH2Data {
 	gchar *custom_client_secret;
 };
 
-gint oauth2_init (OAUTH2Data *OAUTH2Data);
 void oauth2_release(OAUTH2Data *OAUTH2Data);
 gint oauth2_check_passwds (PrefsAccount *ac_prefs);
 gint oauth2_obtain_tokens (Oauth2Service provider, OAUTH2Data *OAUTH2Data, const gchar *authcode);
