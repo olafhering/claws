13b6361c75202373505e87527ba5688720c00cac
Ricardo Mones <ricardo@mones.org>
2026-09-02 13:38:22 +0200

    And yet another constness fix
    filesel.c:122:38: warning: assignment discards 'const' qualifier from pointer target type

--- src/gtk/filesel.c
+++ src/gtk/filesel.c
@@ -114,7 +114,7 @@ static GList *filesel_create(const gchar *title, const gchar *path,
 	gtk_file_chooser_set_select_multiple(GTK_FILE_CHOOSER(chooser), multiple_files);
 
 	if (path && strlen(path) > 0) {
-		char *filename = NULL;
+		const char *filename = NULL;
 		char *realpath = g_strdup(path);
 		char *tmp = NULL;
 		if (path[strlen(path)-1] == G_DIR_SEPARATOR) {
